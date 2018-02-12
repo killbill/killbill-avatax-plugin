07e965e8f42c63af6ec99d4ac5bc4456f88c49fe
Pierre-Alexandre Meyer <pierre@mouraf.org>
2018-02-12 16:08:37 +0000

    Update ddl-postgresql.sql

--- src/main/resources/ddl-postgresql.sql
+++ src/main/resources/ddl-postgresql.sql
@@ -1,6 +1,4 @@
 /* We cannot use timestamp in MySQL because of the implicit TimeZone conversions it does behind the scenes */
-DROP DOMAIN IF EXISTS datetime CASCADE;
 CREATE DOMAIN datetime AS timestamp without time zone;
 
-DROP DOMAIN IF EXISTS longtext CASCADE;
 CREATE DOMAIN longtext AS text;
